a1d4835c6f5d9491d43cfee33f24fd001fe78ee2
LAPTOP-2DMT8PKB\abcd <sergeanttamama@gmail.com>
2021-10-14 00:48:17 +0900

    20211014 주민번호 유효성 체크 함수 추가

---
+++ WonDoLib.pas
@@ -0,0 +1,22 @@
+//공통 라이브러리 작업..!!(Util)
+//2021.10.14 , 원도연 , 최초 작성
+unit WonDoLib;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+function jumin_chk(const s: String): Boolean;
+
+
+implementation
+
+function jumin_chk(const s: String): Boolean;      //2021-10-14 작업중
+begin
+  
+end;
+
+end.
+ 
